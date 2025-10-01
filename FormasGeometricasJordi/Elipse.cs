1993376d671cc6d39ca2d2c873c48590f5194cbb
Ayesta1 <ayesta1@gmail.com>
2025-10-01 10:41:24 +0200

    1.4 - Añadidos metodos en la clase elipse

--- FormasGeometricasJordi/Elipse.cs
+++ FormasGeometricasJordi/Elipse.cs
@@ -8,5 +8,18 @@ namespace FormasGeometricasJordi
 {
     public class Elipse : Forma
     {
+        protected double EjeMayor { get; set; }
+        protected double EjeMenor { get; set; }
+
+        public Elipse(double mayor, double menor)
+        {
+            EjeMayor = mayor;
+            EjeMenor = menor;
+        }
+
+        public override double CalcularArea()
+        {
+            return Math.PI * EjeMayor * EjeMenor;
+        }
     }
 }
